59b08d9befb1a11a97e790ef83524bdd1672823d
afaizal2007 <afaizal2007@gmail.com>
2026-07-17 11:09:13 +0530

    Simplify digit count check for even digits
    Refactor digit count check to use modular arithmetic.

--- Day 1-1295(HW) Find  Number With Even Number Of Digits.java
+++ Day 1-1295(HW) Find  Number With Even Number Of Digits.java
@@ -2,7 +2,6 @@ class Solution {
     public int findNumbers(int[] nums) {
         int count = 0;
         for (int num : nums) {
-            // Check if the number of digits is 2, 4, or 6 based on constraints
             if ((num >= 10 && num <= 99) || 
                 (num >= 1000 && num <= 9999) || 
                 (num == 100000)) {
